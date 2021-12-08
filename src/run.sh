837d858b797e1975bcc7701a5718f6115a4f6ea3
root <jysohn1108@gmail.com>
2021-12-08 20:26:15 +0000

    runninig Q1

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,10 @@
 
 
-python main.py -e fp -i cifar10 -w cifar10 -m matching
+#python main.py -e cliptext -i cifar100 -w cifar100 -m nn
+#python main.py -e cliptext -i cifar100 -w cifar100 -m csls_knn_10
+python main.py -e cliptext -i cifar100 -w cifar100_category -m nn
+python main.py -e fp -i cifar100 -w cifar100_category -m nn
+#python main.py -e fp -i cifar100 -w cifar100_category -m nn
+#python main.py -e fp -i cifar100 -w cifar100_category -m matching
+#python main.py -e fp -i cifar100 -w cifar100 -m matching
+#python main.py -e fp -i cifar10 -w cifar10 -m matching
